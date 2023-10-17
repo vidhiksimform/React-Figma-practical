a912a10756eaba8338f2b9780d337bfb418323c7
vidhiksimform <vidhi.k@simformsolutions.com>
2023-10-17 18:02:34 +0530

    fix: video section for home page

---
+++ src/components/VideoSection.jsx
@@ -0,0 +1,16 @@
+const VideoSection = () => {
+  return (
+    <section className="video-section">
+      <img
+        src="../../../src/assets/video.svg"
+        alt="video"
+        className="videoimg"
+      />
+      <a href="#" className="play-btn">
+        <img src="../../../src/assets/playButton.svg" />
+      </a>
+    </section>
+  );
+};
+
+export default VideoSection;
