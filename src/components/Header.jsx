8eb565802b761fd4802ae7c3c745c06f8aa18022
vidhiksimform <vidhi.k@simformsolutions.com>
2023-10-17 17:42:06 +0530

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { navigationMenu } from "../utils";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="../../src/assets/logo.svg" alt="Logo appex" className="logo" />
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          {navigationMenu.map(({ menuName, redirectRoute }, index) => (
+            <li key={index}>
+              <a className="main-nav-link" href={`/${redirectRoute}`}>
+                {menuName}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
